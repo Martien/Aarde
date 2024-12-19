3a250e5df1fe889f90aa8da9aecdebae850c206d
Martien van Steenbergen <martien@aardrock.com>
2024-12-19 13:26:43 +0100

    += simplexity.md + output outside cwd

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,6 +1,8 @@
 import { markdownIt } from "./markdown-it-wiki-pearl.js";
 import { pages } from "./src/zones/zones.js";
 
+const output = `/Users/martien/Documents/nl.aardrock.aarde/${((now) => new Date(now - 60000 * now.getTimezoneOffset()).toISOString().slice(0, -1))(new Date())}`;
+
 export default {
   title: "Aarde",
   pages,
@@ -8,6 +10,7 @@ export default {
   root: "src",
   search: true,
   style: "style.css",
+  output,
   footer: ({ path }) =>
     `Built with <a href="https://observablehq.com/">Observable</a> – <a href="https://github.com/Martien/aarde/blob/main/src/${path}.md?plain=1">view page source</a>`,
   markdownIt,
---
+++ src/simplexity.md
@@ -0,0 +1,15 @@
+---
+title: Simplexity
+date: 2008-02-01
+categories: 
+ - "zomaar"
+---
+
+# Simplexity
+### 2008-02-01
+
+De door TNO, Oxrider en Aardbron georganiseerde samenscholing op 1 februari over Simplicity of Complexity in het wondermooie, volgens Feng Shui gebouwde, Bomencentrum in Baarn was een prachtig succes.
+
+De heldere uiteenzetting van Francis Heijligers en de prachtige natuurfotografie waren een lust voor zowel "mind" als "eye".
+
+[Mijn aantekeningen van deze dag staan op de Wiki](http://water.aardbron.nl/Simplicity_of_Complexity).
